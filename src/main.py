7e681cb7fd6c64d0aa770f398513d543d8008cf7
OmegaZerg <ragged17@Bellsouth.net>
2025-04-27 15:36:35 -0500

    Tons of work done inside the main.py file. Lots of testing over the course of the day to verify functionality at each step. Essentially we have a move directory function that will check for a vlid source/destination, then clear the destination directory if it exists. Next it will create the destination directory, then call the copy_source function. Copy source will evaluate the contents and copy them over appropriately, depending on if they are a file or another directory. If a directory, a recursive call is made to run through another iteration and continue until all files/directories have been copied. Logging is added at each step, refactoring and some cleanup may still be needed. But for today...I am beat.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from os.path import join, exists, isfile
+from os.path import join, exists, isfile, isdir
 from os import listdir, mkdir
 import sys
 import shutil
@@ -7,19 +7,29 @@ logging.basicConfig(level=logging.INFO, filename="main.log", filemode="w", forma
 
 #Helper function that will copy a source directory and everything it contains.
 def copy_source(source_directory, destination_directory):
-    logging.info(f"Creating new directory < {destination_directory} >...")
-    try:
-        mkdir(destination_directory)
-    except FileExistsError as e:
-        logging.info(f"Exception thrown: unable to create directory. Reason: {e}")
-        sys.exit()
-
-    if not exists(join(destination_directory)):
-        logging.info(f"Destination directory failed to be created!")
-    logging.info(f"Destination directory < {destination_directory} > was successfully created!")
-
+    if not source_directory:
+        return
     contents = listdir(source_directory)
     logging.info(f"Contents found at source directory: {contents}")
+    for item in contents:
+        source_path = join(source_directory, item)
+        destination_path = join(destination_directory, item)
+        if isfile(source_path):
+            logging.info(f"File found! Copying {item} from <{source_path}> to <{destination_path}>")
+            shutil.copy(source_path, destination_path)
+        elif isdir(source_path):
+            logging.info(f"Directory found! First creating source directory...")
+            try:
+                mkdir(destination_path)
+                logging.info("Directory created!")
+            except FileExistsError as e:
+                logging.info(f"Exception thrown: unable to create directory. Reason: {e}")
+                sys.exit()
+            logging.info(f"Performing black hole recursion...")
+            copy_source(source_path, destination_path)
+    logging.info("Checking to see if we made it out ok...")
+    
+
 
 #Function that copies all the contents from a source directory to a destination directory
 #!Warning: This function will delete all contents of the destination directory prio to the move
@@ -30,7 +40,7 @@ TODO: 3. Create helper function that logs each copy action
 TODO: 4. Copy source directory and all contents to the destination directory
 """
 def move_directory(source, destination):
-    check_path = (exists(join(source)), exists(join(destination)))
+    check_path = (exists(source), exists(destination))
     logging.info(f"source exists: {check_path[0]} ||destination exists: {check_path[1]}")
     if False in check_path:
         logging.info(f"Either the source or destination is invalid, exiting...")
@@ -46,12 +56,22 @@ def move_directory(source, destination):
     logging.info(f"Deletion event completed on the <{destination}> directory.")
     logging.info(f"Is the destination still there after deletion? {exists(destination)}")
 
-    logging.info(f"Attempting to copy from < {source} >")
+    logging.info(f"Attempting to copy from <{source}>")
+    logging.info(f"Creating new directory <{destination}>...")
+    try:
+        mkdir(destination)
+    except FileExistsError as e:
+        logging.info(f"Exception thrown: unable to create directory. Reason: {e}")
+        sys.exit()
+
+    if not exists(destination):
+        logging.info(f"Destination directory failed to be created!")
+    logging.info(f"Destination directory <{destination}> was successfully created!")
     copy_source(source, destination)
 
 
 
 def main():
-    move_directory("static", "test")
+    move_directory("static", "public")
 
 main()
