49fc74cdd5f0a495cad179560e37b13c727474ae
OmegaZerg <ragged17@Bellsouth.net>
2025-04-27 11:50:16 -0500

    Added some module imports that were required, started building out the move_directory function. Added a bunch of loggers and starting to test directory deletion functionality.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-
+from os.path import join, exists, isfile
+import sys
+import shutil
+import logging
+logging.basicConfig(level=logging.INFO, filename="main.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
 
 #Function that copies all the contents from a source directory to a destination directory
 #!Warning: This function will delete all contents of the destination directory prio to the move
@@ -9,9 +13,26 @@ TODO: 3. Create helper function that logs each copy action
 TODO: 4. Copy source directory and all contents to the destination directory
 """
 def move_directory(source, destination):
-    pass
+    test_source = join(source)
+    logging.info(f"source format: {test_source}")
+    test_destination = join(destination)
+    logging.info(f"destination format: {test_destination}")
+    check_path = (exists(test_source), exists(test_destination))
+    logging.info(f"source exists: {check_path[0]} ||destination exists: {check_path[1]}")
+    logging.info(f"attempting to clear destination directory <{destination}>")
+    try:
+        shutil.rmtree(destination)
+    except FileNotFoundError as e:
+        logging.info(f"Exception thrown: unable to delete directory. Reason: {e}")
+        sys.exit()
+
+    logging.info(f"Deletion event completed on the<{destination}> directory.")
+    check_destination = exists(destination)
+    logging.info(f"Is the destination still there after deletion? {check_destination}")
+
+
 
 def main():
-    pass
+    move_directory("static", "test")
 
 main()
